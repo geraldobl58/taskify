fce0e03284e9542925cc41c1d870b722b85a22d4
Geraldo Luiz <geraldobl58@gmail.com>
2024-05-07 17:54:23 -0300

    feat: Add components in page

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,15 @@
-const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+
+const OrganizationIdPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <BoardList />
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
