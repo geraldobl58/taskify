167ef7ad1787ba49dd04200ff2c46b5486735674
Geraldo Luiz <geraldobl58@gmail.com>
2024-05-21 18:04:52 -0300

    feat: Add list container component

---
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { ListWithCards } from "@/lib/types";
+
+import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
+
+interface ListContainerProps {
+  boardId: string;
+  data: ListWithCards[];
+}
+
+export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
+  return (
+    <ol className="flex h-full gap-x-3">
+      {orderedData.map((list, index) => (
+        <ListItem key={list.id} index={index} data={list} />
+      ))}
+      <ListForm />
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  );
+};
