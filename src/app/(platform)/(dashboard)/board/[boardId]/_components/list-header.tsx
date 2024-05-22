5a1af1586b94af6f64bd88ff9aade2a04689c0e1
Geraldo Luiz <geraldobl58@gmail.com>
2024-05-22 12:03:07 -0300

    feat: Add list header component

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -18,9 +18,10 @@ import { useAction } from "@/hooks/use-action";
 
 interface ListHeaderProps {
   data: List;
+  onAddCard: () => void;
 }
 
-export const ListHeader = ({ data }: ListHeaderProps) => {
+export const ListHeader = ({ data, onAddCard }: ListHeaderProps) => {
   const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -122,7 +123,7 @@ export const ListHeader = ({ data }: ListHeaderProps) => {
           {title}
         </div>
       )}
-      <ListOptions data={data} onAddCard={() => {}} />
+      <ListOptions data={data} onAddCard={onAddCard} />
     </div>
   );
 };
